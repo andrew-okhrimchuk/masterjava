0b407a24a869a4b0edc206afe130ccc0620dca12
andrew-okhrimchuk <andrew456gold@gmail.com>
2019-01-20 10:12:26 +0200

    HW 1. changed singleThreadMultiply method

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -20,18 +20,42 @@ public class MatrixUtil {
 
     // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
-        final int matrixSize = matrixA.length;
-        final int[][] matrixC = new int[matrixSize][matrixSize];
+        final int aColumns = matrixA[0].length;
+        final int aRows = matrixA.length;
+        final int bRows = matrixB.length;
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
-                for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+        int thatColumn[] = new int[bRows];
+
+        int[][] matrixC = new int[bRows][bRows];
+        try {
+            for (int j = 0; ; j++) {
+                for (int k = 0; k < aColumns; k++) {
+                    thatColumn[k] = matrixB[k][j];
                 }
-                matrixC[i][j] = sum;
+
+                for (int i = 0; i < aRows; i++) {
+                    int thisRow[] = matrixA[i];
+                    int summand = 0;
+                    for (int k = 0; k < aColumns; k++) {
+                        summand += thisRow[k] * thatColumn[k];
+                    }
+                    matrixC[i][j] = summand;
+                }
             }
+        } catch (IndexOutOfBoundsException ignored) { }
+
+       /* for (int i = 0; i < matrixA.length; i++) {
+            System.out.println(Arrays.toString(matrixA[i]));
         }
+        System.out.println("--------------------");
+        for (int i = 0; i < matrixB.length; i++) {
+            System.out.println(Arrays.toString(matrixB[i]));
+        }
+        System.out.println("--------------------");
+
+        for (int i = 0; i < matrixC.length; i++) {
+            System.out.println(Arrays.toString(matrixC[i]));
+        }*/
         return matrixC;
     }
 
