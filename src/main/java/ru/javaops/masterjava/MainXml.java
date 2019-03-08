9ce6804666d4151192a667907abcc3d5189e1b6f
andrew-okhrimchuk <andrew456gold@gmail.com>
2019-03-08 09:50:57 +0200

    crated MainXml.

---
+++ src/main/java/ru/javaops/masterjava/MainXml.java
@@ -0,0 +1,50 @@
+package ru.javaops.masterjava;
+
+import com.google.common.io.Resources;
+import org.xml.sax.SAXException;
+import ru.javaops.masterjava.xml.schema.Group;
+import ru.javaops.masterjava.xml.schema.ObjectFactory;
+import ru.javaops.masterjava.xml.schema.Payload;
+import ru.javaops.masterjava.xml.util.JaxbParser;
+import ru.javaops.masterjava.xml.util.Schemas;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
+
+public class MainXml {
+    private static final JaxbParser JAXB_PARSER = new JaxbParser(ObjectFactory.class);
+    private String nameProject;
+
+    static {
+        JAXB_PARSER.setSchema(Schemas.ofClasspath("payload.xsd"));
+    }
+
+    public MainXml(String nameProject) throws JAXBException, IOException, SAXException {
+        this.nameProject = nameProject;
+       }
+
+    public static void main(String[] args) throws JAXBException, IOException, SAXException {
+       /* MainXml xx = new MainXml("topjava");
+        MainXml xx2 = new MainXml("masterjava");
+        xx.print();
+        xx2.print();*/
+    }
+
+    public void print () throws IOException, JAXBException, SAXException {
+        Payload payload = JAXB_PARSER.unmarshal(
+                Resources.getResource("payload.xml").openStream());
+        String strPayload = JAXB_PARSER.marshal(payload);
+        JAXB_PARSER.validate(strPayload);
+        List<String> xx = payload.getProjects().getProject()
+                .stream()
+                .filter(project -> project.getName().equalsIgnoreCase(nameProject))
+                .map(project -> project.getName())
+                .map(name -> {System.out.println(name); return name;})
+                .collect(toList());
+
+    }
+}
---
+++ src/main/java/ru/javaops/masterjava/xml/schema/FlagType2.java
@@ -0,0 +1,40 @@
+
+package ru.javaops.masterjava.xml.schema;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for flagType2.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="flagType2">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="REGISTERING"/>
+ *     &lt;enumeration value="CURRENT"/>
+ *     &lt;enumeration value="FINISHED"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "flagType2", namespace = "http://javaops.ru")
+@XmlEnum
+public enum FlagType2 {
+
+    REGISTERING,
+    CURRENT,
+    FINISHED;
+
+    public String value() {
+        return name();
+    }
+
+    public static FlagType2 fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
